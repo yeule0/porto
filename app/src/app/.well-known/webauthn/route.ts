3cc8903010573b0bbd11e1ecc3f706d1424b9421
jxom <7336481+jxom@users.noreply.github.com>
2024-11-14 14:46:53 +1100

    feat: init app (for universal webauthn keys)

---
+++ app/src/app/.well-known/webauthn/route.ts
@@ -0,0 +1,9 @@
+export async function GET(_: Request) {
+  return Response.json({
+    origins: ['TODO'],
+  })
+}
+
+export async function UPDATE(_: Request) {
+  return Response.json({ data: 'TODO' })
+}
---
+++ app/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-[#145AC6] min-h-[100dvh] flex items-center justify-center">
+      <img src="/mark-white.svg" alt="Oddworld" width={60} height={60} />
+    </div>
+  )
+}
