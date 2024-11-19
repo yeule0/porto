49047f7dc9262f423278ca2567227c6044373e1f
jxom <7336481+jxom@users.noreply.github.com>
2024-11-19 16:34:42 +1100

    chore: createStore

--- src/internal/storage.ts
+++ src/internal/storage.ts
@@ -1,17 +1,19 @@
-import { del, get, set } from 'idb-keyval'
+import { createStore, del, get, set } from 'idb-keyval'
 import type { PersistStorage } from 'zustand/middleware'
 import type { State } from '../Oddworld.js'
 
+const store = createStore('oddworld', 'store')
+
 export const idb = {
   async getItem(name) {
-    const value = await get(name)
+    const value = await get(name, store)
     if (value === null) return null
     return value
   },
   async removeItem(name) {
-    await del(name)
+    await del(name, store)
   },
   async setItem(name, value) {
-    await set(name, value)
+    await set(name, value, store)
   },
 } satisfies PersistStorage<State>
