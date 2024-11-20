43cdca307055a020baf8d19c960fa8fd4bbf10a8
jxom <7336481+jxom@users.noreply.github.com>
2024-11-21 08:37:45 +1100

    chore: up

--- src/Porto.ts
+++ src/Porto.ts
@@ -13,7 +13,7 @@ export const defaultConfig = {
   announceProvider: true,
   chains: [Chains.odysseyTestnet],
   headless: true,
-  keystoreHost: 'porto-tau.vercel.app',
+  keystoreHost: 'exp.porto.sh',
   transports: {
     [Chains.odysseyTestnet.id]: http(),
   },
