50fca67949c2f056138b6182eeef8951af27297c
jxom <7336481+jxom@users.noreply.github.com>
2025-01-24 10:46:16 +1100

    fix: dialog width

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -39,40 +39,39 @@ function RouteComponent() {
   }, [mode])
 
   return (
-    <div className="max-w-[282px]" ref={elementRef}>
-      <div
-        {...{ [`data-${mode}`]: '' }} // for conditional styling based on dialog mode ("in-data-iframe:..." or "in-data-popup:...")
-        className="flex h-fit min-w-[282px] flex-col overflow-hidden border-blackA1 bg-gray1 data-iframe:rounded-[14px] data-iframe:border dark:border-whiteA1 dark:bg-gray2"
-      >
-        <header className="flex items-center justify-between border-blackA1 border-b bg-blackA1 px-3 pt-2 pb-1.5 dark:border-whiteA1 dark:bg-whiteA1">
-          <div className="flex items-center gap-2">
-            <div className="flex size-5 items-center justify-center rounded-[5px] bg-gray6">
-              <LucideGlobe className="size-3.5 text-black dark:text-white" />
-            </div>
-            <div className="font-normal text-[14px] text-gray9 leading-[22px]">
-              {hostname}
-            </div>
+    <div
+      ref={elementRef}
+      {...{ [`data-${mode}`]: '' }} // for conditional styling based on dialog mode ("in-data-iframe:..." or "in-data-popup:...")
+      className="flex h-fit min-w-dialog flex-col overflow-hidden border-blackA1 bg-gray1 data-iframe:max-w-dialog data-iframe:rounded-[14px] data-iframe:border dark:border-whiteA1 dark:bg-gray2"
+    >
+      <header className="flex items-center justify-between border-blackA1 border-b bg-blackA1 px-3 pt-2 pb-1.5 dark:border-whiteA1 dark:bg-whiteA1">
+        <div className="flex items-center gap-2">
+          <div className="flex size-5 items-center justify-center rounded-[5px] bg-gray6">
+            <LucideGlobe className="size-3.5 text-black dark:text-white" />
           </div>
+          <div className="font-normal text-[14px] text-gray9 leading-[22px]">
+            {hostname}
+          </div>
+        </div>
 
-          <button
-            type="button"
-            onClick={() => {
-              const state = requestsStore.getState()
-              const request = state.requests[0]
-              if (request) state.reject(request)
-            }}
-            title="Close Dialog"
-          >
-            <LucideX className="size-4.5 text-gray9" />
-          </button>
-        </header>
+        <button
+          type="button"
+          onClick={() => {
+            const state = requestsStore.getState()
+            const request = state.requests[0]
+            if (request) state.reject(request)
+          }}
+          title="Close Dialog"
+        >
+          <LucideX className="size-4.5 text-gray9" />
+        </button>
+      </header>
 
-        <Outlet />
+      <Outlet />
 
-        <React.Suspense>
-          <TanStackRouterDevtools position="bottom-right" />
-        </React.Suspense>
-      </div>
+      <React.Suspense>
+        <TanStackRouterDevtools position="bottom-right" />
+      </React.Suspense>
     </div>
   )
 }
