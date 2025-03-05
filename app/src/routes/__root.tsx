81542676f4f23238075825d2fe4cd244fd3859a8
jxom <7336481+jxom@users.noreply.github.com>
2025-03-06 06:31:23 +1100

    fix: messenger race condition

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -1,11 +1,19 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import * as React from 'react'
 
+import { porto } from '../lib/Porto'
+
 export const Route = createRootRoute({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  React.useEffect(() => {
+    // Note: we already call `porto.ready()` optimistically in `main.tsx`, but
+    // we should call it here incase it didn't resolve due to a race condition.
+    porto.ready()
+  }, [])
+
   return (
     <>
       <Outlet />
--- src/core/Dialog.ts
+++ src/core/Dialog.ts
@@ -113,6 +113,7 @@ export function iframe() {
         to: Messenger.fromWindow(iframe.contentWindow!, {
           targetOrigin: hostUrl.origin,
         }),
+        waitForReady: true,
       })
 
       messenger.on('ready', () => {
