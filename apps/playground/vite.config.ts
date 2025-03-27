96324275f98d3acd3070890c0ae4833af271e6f0
jxom <7336481+jxom@users.noreply.github.com>
2025-03-27 16:43:22 +1100

    chore: up playground

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -8,14 +8,13 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define:
-    process.env.NODE_ENV === 'development'
-      ? {
-          'import.meta.env.VITE_DIALOG_HOST': JSON.stringify(
-            'https://localhost:5174/dialog/',
-          ),
-        }
-      : undefined,
+  define: {
+    'import.meta.env.VITE_DIALOG_HOST': JSON.stringify(
+      process.env.NODE_ENV === 'development'
+        ? 'https://localhost:5174/dialog/'
+        : 'https://id.porto.sh/dialog/',
+    ),
+  },
   plugins: [
     mkcert(),
     react(),
