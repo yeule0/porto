a93c68f6bfc85c0ef861ac4760cc0ca2a1c0360b
jxom <7336481+jxom@users.noreply.github.com>
2025-03-27 18:40:54 +1100

    fix: types

--- apps/docs/vite-env.d.ts
+++ apps/docs/vite-env.d.ts
@@ -1,8 +1,6 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv extends Environment {
-  readonly VITE_DIALOG_HOST: string
-}
+interface ImportMetaEnv extends Environment {}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
