df65d3a77e6be728ff4f31d0840ed418daff5d8d
jxom <7336481+jxom@users.noreply.github.com>
2025-03-31 14:52:47 +1100

    chore: tweaks

--- apps/dialog/src/main.tsx
+++ apps/dialog/src/main.tsx
@@ -24,7 +24,7 @@ const offInitialized = Events.onInitialized(porto, (payload) => {
 
 const offRequests = Events.onRequests(porto, (requests) => {
   const request = requests[0]?.request
-  if (!Dialog_porto.requiresConfirmation(request as never)) {
+  if (request && !Dialog_porto.requiresConfirmation(request)) {
     Actions.respond(porto, requests[0]!)
     return
   }
