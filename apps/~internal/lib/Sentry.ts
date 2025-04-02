c9d6263bc01d15d78b231c6b2068f09745b7c7ee
jxom <7336481+jxom@users.noreply.github.com>
2025-04-02 20:34:21 +1100

    chore: rm console.log

--- apps/~internal/lib/Sentry.ts
+++ apps/~internal/lib/Sentry.ts
@@ -2,10 +2,8 @@ import * as Sentry from '@sentry/react'
 import { HttpTransportConfig } from 'viem'
 
 export function httpTransportOptions(): HttpTransportConfig {
-  console.log('httpTransportOptions')
   return {
     async onFetchRequest(request, init) {
-      console.log(request.url, typeof init.body)
       const span = Sentry.getActiveSpan()
       span?.setAttribute('network.request.url', request.url)
       span?.setAttribute('network.request.body', init.body as string)
