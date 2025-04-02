eb8b04f968c0cf9697b5d4e58dd0ebee50eb75f2
jxom <7336481+jxom@users.noreply.github.com>
2025-04-02 20:57:03 +1100

    chore: up

--- apps/~internal/lib/Sentry.ts
+++ apps/~internal/lib/Sentry.ts
@@ -4,16 +4,27 @@ import { HttpTransportConfig } from 'viem'
 export function httpTransportOptions(): HttpTransportConfig {
   return {
     async onFetchRequest(request, init) {
-      const span = Sentry.getActiveSpan()
-      span?.setAttribute('network.request.url', request.url)
-      span?.setAttribute('network.request.body', init.body as string)
+      Sentry.addBreadcrumb({
+        category: 'network.request',
+        data: {
+          body: init.body,
+          url: request.url,
+        },
+        level: 'info',
+        message: 'RPC Request',
+      })
     },
     async onFetchResponse(response) {
-      const span = Sentry.getActiveSpan()
-      span?.setAttribute(
-        'network.response',
-        JSON.stringify(await response.clone().json()),
-      )
+      const data = await response.clone().json()
+      Sentry.addBreadcrumb({
+        category: 'network.response',
+        data: {
+          data,
+          url: response.url,
+        },
+        level: 'info',
+        message: 'RPC Response',
+      })
     },
   }
 }
