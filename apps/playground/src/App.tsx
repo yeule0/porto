29b48e3de8cba71e18be7900472e131bf49f499b
omar <omaraziz.dev@proton.me>
2025-05-29 16:09:32 -0700

    chore(playground): log & show errors in page for mobile debug (#362)

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -185,6 +185,8 @@ function Events() {
 function Connect() {
   const [grantPermissions, setGrantPermissions] = React.useState<boolean>(false)
   const [result, setResult] = React.useState<unknown | null>(null)
+  const [error, setError] = React.useState<string | null>(null)
+
   return (
     <div>
       <h3>wallet_connect</h3>
@@ -198,51 +200,59 @@ function Connect() {
       </label>
       <div>
         <button
-          onClick={() =>
-            porto.provider
+          onClick={() => {
+            const payload = {
+              capabilities: {
+                createAccount: false,
+                grantPermissions: grantPermissions ? permissions() : undefined,
+              },
+            } as const
+            return porto.provider
               .request({
                 method: 'wallet_connect',
-                params: [
-                  {
-                    capabilities: {
-                      createAccount: false,
-                      grantPermissions: grantPermissions
-                        ? permissions()
-                        : undefined,
-                    },
-                  },
-                ],
+                params: [payload],
               })
               .then(setResult)
-          }
+              .catch((error) => {
+                console.error(error)
+                setError(
+                  Json.stringify({ error: error.message, payload }, null, 2),
+                )
+              })
+          }}
           type="button"
         >
           Login
         </button>
         <button
-          onClick={() =>
-            porto.provider
+          onClick={() => {
+            const payload = {
+              capabilities: {
+                createAccount: true,
+                grantPermissions: grantPermissions ? permissions() : undefined,
+              },
+            } as const
+
+            return porto.provider
               .request({
                 method: 'wallet_connect',
-                params: [
-                  {
-                    capabilities: {
-                      createAccount: true,
-                      grantPermissions: grantPermissions
-                        ? permissions()
-                        : undefined,
-                    },
-                  },
-                ],
+                params: [payload],
               })
               .then(setResult)
-          }
+              .catch((error) => {
+                console.error(error)
+                setError(
+                  Json.stringify({ error: error.message, payload }, null, 2),
+                )
+              })
+          }}
           type="button"
         >
           Register
         </button>
       </div>
       {result ? <pre>{JSON.stringify(result, null, 2)}</pre> : null}
+      {error ? <pre>{error}</pre> : null}
     </div>
   )
 }
