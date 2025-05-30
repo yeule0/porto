56217f361151722131fc4fff28103cae23d6235d
jxom <7336481+jxom@users.noreply.github.com>
2025-05-30 11:42:23 +0200

    tests: fix browser mode

--- test/globalSetup.browser.ts
+++ test/globalSetup.browser.ts
@@ -11,7 +11,7 @@ export default async function () {
           env: {
             ...(process as any).env,
             ...import.meta.env,
-            ANVIL: 'true',
+            ANVIL: (import.meta.env.VITE_LOCAL !== 'false').toString(),
           },
         },
       )
