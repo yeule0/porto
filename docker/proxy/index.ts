0c320ab0608d6419d0ad7d9aed3aece2a05dcb46
jxom <7336481+jxom@users.noreply.github.com>
2025-06-02 14:52:13 +0200

    up

--- docker/proxy/index.ts
+++ docker/proxy/index.ts
@@ -3,7 +3,7 @@ Bun.serve({
     const body = await req.json()
     const target =
       body.method &&
-      (body.method.startsWith('relay_') || body.method === 'health')
+      (body.method.startsWith('wallet_') || body.method === 'health')
         ? Bun.env.RELAY_URL!
         : Bun.env.ANVIL_URL!
     return fetch(target, {
