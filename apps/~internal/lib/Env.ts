09113c584463be939732feb94c6dc85ff2283b87
jxom <7336481+jxom@users.noreply.github.com>
2025-06-04 13:03:16 +0200

    chore: tweak

--- apps/~internal/lib/Env.ts
+++ apps/~internal/lib/Env.ts
@@ -4,7 +4,8 @@ export const defaultEnv = (() => {
     return 'dev'
   }
   if (import.meta.env.VITE_VERCEL_ENV === 'preview') return 'dev'
-  if (import.meta.env.VITE_DEFAULT_ENV) return import.meta.env.VITE_DEFAULT_ENV
+  if (import.meta.env.VITE_DEFAULT_ENV)
+    return import.meta.env.VITE_DEFAULT_ENV as Env
   return 'stg'
 })()
 
@@ -15,7 +16,8 @@ export function get(): Env {
   if (typeof window === 'undefined') return defaultEnv
 
   const url = new URL(window.location.href)
-  const env = url.searchParams.get('env') ?? window.location.host.split('.')[0]
+  const env =
+    url.searchParams.get('env') ?? window.location.host.split(/\.|-/)[0]
   if (env && envs.includes(env as Env)) return env as Env
 
   return defaultEnv
