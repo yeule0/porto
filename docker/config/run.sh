7dd1f84aba2cc133845e4c9c2f585a7a6485d698
jxom <7336481+jxom@users.noreply.github.com>
2025-06-09 17:03:56 +0200

    chore(docker): rm account registry

--- docker/config/run.sh
+++ docker/config/run.sh
@@ -2,7 +2,6 @@ ANVIL_ADDRESS=0xa0Ee7A142d267C1f36714E4a8F75612F20a79720
 ANVIL_PRIVATE_KEY=0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6
 RPC_URL=http://anvil:8545
 
-ACCOUNT_REGISTRY_ADDRESS=$(forge create AccountRegistry --config-path ./account/foundry.toml --json --broadcast --rpc-url $RPC_URL --private-key $ANVIL_PRIVATE_KEY | jq -r '.deployedTo')
 ORCHESTRATOR_ADDRESS=$(forge create Orchestrator --config-path ./account/foundry.toml --json --broadcast --rpc-url $RPC_URL --private-key $ANVIL_PRIVATE_KEY --constructor-args 0x0000000000000000000000000000000000000000 | jq -r '.deployedTo')
 ACCOUNT_ADDRESS=$(forge create IthacaAccount --config-path ./account/foundry.toml --json --broadcast --rpc-url $RPC_URL --private-key $ANVIL_PRIVATE_KEY --constructor-args $ORCHESTRATOR_ADDRESS | jq -r '.deployedTo')
 ACCOUNT_PROXY_ADDRESS=$(forge create EIP7702Proxy --config-path ./account/foundry.toml --json --broadcast --rpc-url $RPC_URL --private-key $ANVIL_PRIVATE_KEY --constructor-args $ACCOUNT_ADDRESS $ANVIL_ADDRESS | jq -r '.deployedTo')
@@ -11,7 +10,6 @@ SIMULATOR_ADDRESS=$(forge create Simulator --config-path ./account/foundry.toml 
 EXP1_ADDRESS=$(forge create ExperimentERC20 --config-path ./demo/foundry.toml --json --broadcast --rpc-url $RPC_URL --private-key $ANVIL_PRIVATE_KEY --constructor-args "ExperimentERC20" "EXP" 1ether | jq -r '.deployedTo')
 EXP2_ADDRESS=$(forge create ExperimentERC20 --config-path ./demo/foundry.toml --json --broadcast --rpc-url $RPC_URL --private-key $ANVIL_PRIVATE_KEY --constructor-args "ExperimentERC20" "EXP2" 10ether | jq -r '.deployedTo')
 
-echo "AccountRegistry deployed to: $ACCOUNT_REGISTRY_ADDRESS"
 echo "Orchestrator deployed to: $ORCHESTRATOR_ADDRESS"
 echo "Account deployed to: $ACCOUNT_ADDRESS"
 echo "EIP7702Proxy deployed to: $ACCOUNT_PROXY_ADDRESS"
@@ -23,7 +21,6 @@ echo "ExperimentERC20 deployed to: $EXP2_ADDRESS"
 cp /app/relay.yaml /app/shared/relay.yaml
 
 yq -i ".orchestrator = \"$ORCHESTRATOR_ADDRESS\"" /app/shared/relay.yaml
-yq -i ".account_registry = \"$ACCOUNT_REGISTRY_ADDRESS\"" /app/shared/relay.yaml
 yq -i ".delegation_proxy = \"$ACCOUNT_PROXY_ADDRESS\"" /app/shared/relay.yaml
 yq -i ".simulator = \"$SIMULATOR_ADDRESS\"" /app/shared/relay.yaml
 
