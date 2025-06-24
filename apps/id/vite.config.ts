27092290919530cb54eb8c312c4e876d3ee6ae54
o-az <omaraziz.dev@proton.me>
2025-06-24 16:12:27 -0700

    chore: promote in vercel (#509)

--- apps/id/vite.config.ts
+++ apps/id/vite.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, loadEnv } from 'vite'
 import Mkcert from 'vite-plugin-mkcert'
 import TsconfigPaths from 'vite-tsconfig-paths'
 
-const commitSha =
+const portoCommitSha =
   ChildProcess.execSync('git rev-parse --short HEAD').toString().trim() ||
   process.env.VERCEL_GIT_COMMIT_SHA?.slice(0, 7)
 
@@ -32,7 +32,7 @@ export default defineConfig(({ mode }) => {
       sourcemap: true,
     },
     define: {
-      __APP_VERSION__: JSON.stringify(commitSha),
+      __APP_VERSION__: JSON.stringify(portoCommitSha),
       'process.env': {},
     },
     plugins: [
